d4d429306fe7794db549a5b8f6eac914c134c215
viliuspc <viliuspc@debian.vilius>
2024-03-17 17:15:43 +0200

    (test) attempt cf pages deployment

--- tina/config.tsx
+++ tina/config.tsx
@@ -8,6 +8,7 @@ const config = defineConfig({
   branch:
     process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+    process.env.CF_PAGES_BRANCH ||
     process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
   media: {
