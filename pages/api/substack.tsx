e33cbc7d9dadbb6221120d113094795b4bfe9edc
viliuspc <viliuspc@debian.vilius>
2024-03-19 17:40:53 +0200

    adjust substack article api route for edge param, add button titles on arrow elems

--- pages/api/substack.tsx
+++ pages/api/substack.tsx
@@ -1,16 +1,17 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 export const runtime = 'edge';
 
 const API_URL = "https://skirmantesolutionfocus.substack.com/api/v1/archive";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+export default async function handler() {
   try {
     const response = await fetch(`${API_URL}?limit=5&sort=new`);
     if (!response.ok) throw new Error("Failed to fetch substack articles");
+
     const data = await response.json();
-    res.status(200).json(data);
+    return NextResponse.json(data);
   } catch (e) {
-      res.status(500).json({ ok: false, error: "Failed to fetch substack articles" });
+      return NextResponse.json({ ok: false, error: "Failed to fetch substack articles"});
   }
 }
